152b127b07b53ea74f7a3d03fe601b9d19cda182
jokerRz <1184953272@qq.com>
2022-05-20 02:29:29 +0800

    httpcomm:1 upload utils:4

--- comm_impl/httpcomm/httpcomm/util/thread.h
+++ comm_impl/httpcomm/httpcomm/util/thread.h
@@ -61,7 +61,47 @@ private:
 
 class Cond {
 public:
-  explicit Cond(Mutex &mutex) : mutex_(mutex)
+  explicit Cond(Mutex &mutex) : mutex_(mutex) {
+    pthread_cond_init(&cond_, NULL);
+  }
+
+  ~Cond() { pthread_cond_destroy(&cond_); }
+
+  bool Wait(int64_t us);
+
+  bool Signal() { return pthread_cond_signal(&cond_) == 0; }
+
+  bool BroadCast() { return pthread_cond_broadcast(&cond_) == 0; }
+
+private:
+  pthread_cond_t cond_;
+  Mutex& mutex_;
+
+  HTTPCOMM_DISABLE_COPY_ASSIGN(Cond);
+};
+
+class ScopedLock {
+public:
+  explicit ScopedLock(Mutex& mutex) : mutex_(mutex),
+                                      enable_(true) {
+    mutex_.Lock();
+  }
+
+  ~ScopedLock() {
+    if (enable_) {
+      mutex_.Unlock();
+    }
+  }
+
+  void Disable() {
+    enable_ = false;
+  }
+
+private:
+  Mutex& mutex_;
+  bool enable_;
+
+  HTTPCOMM_DISABLE_COPY_ASSIGN(ScopedLock);
 };
 
 HTTPCOMM_NAMESPACE_END
