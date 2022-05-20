237a53c758c932bbf521f1e891cd9a53b4cd3ebc
jokerRz <1184953272@qq.com>
2022-05-20 13:48:25 +0800

    httpcomm:1 upload utils:5

--- comm_impl/httpcomm/httpcomm/util/thread.cc
+++ comm_impl/httpcomm/httpcomm/util/thread.cc
@@ -2,6 +2,68 @@
 
 HTTPCOMM_NAMESPACE_BEGIN
 
+namespace {
 
+void* ThreadHook(void* userData) {
+  assert(userData != NULL);
+  Thread* thread = dynamic_cast<Thread*>(userData);
+  const Thread::ThreadFunc& func = thread->GetThreadFunc();
+  if (func) {
+    func();
+  }
+  return NULL;
+}
+
+} // namespace
+
+Thread::Thread() : tid_(0),
+                   started_(false) {
+}
+
+Thread::Thread(cosnt ThreadFunc& threadFunc) : tid_(0),
+                                               thread_func_(threadFunc),
+                                               started_(false) {
+}
+
+Thread::~Thread() {}
+
+bool Thread::Start() {
+  if (started || !thread_func_) {
+    return false;
+  }
+  int ret = pthread_create(&tid_, NULL, ThreadHook, this);
+  started_ = ret == 0;
+  return started_;
+}
+
+bool Thread::Join() {
+  if (!started_) {
+    return false;
+  }
+  return pthread_join(tid_, NULL) == 0;
+}
+
+bool Thread::Detach() {
+  if (!started_) {
+    return false;
+  }
+  return pthread_detach(tid_) == 0;
+}
+
+void Thread::SetThreadFunc(ThreadFunc& threadFunc) {
+  thread_func_ = threadFunc;
+}
+
+const ThreadFunc& Thread::GetThreadFunc() {
+  return thread_func_;
+}
+
+bool Cond::Wait(int64_t us) {
+  if (us == 0LL) {
+    return pthread_cond_wait(&cond_, &mutex_.mutex_) == 0;
+  }
+  struct timespec ts = TimeUtil::GetTimeSpec(us);
+  return pthread_cond_timewait(&cond_, &mutex_.mutex, &ts) == 0;
+}
 
 HTTPCOMM_NAMESPACE_END
